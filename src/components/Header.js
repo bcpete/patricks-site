d3a60b206ae3f4303514f9d7e3f9dd79e0d9f2af
hometownzero <bradycpeters@gmail.com>
2018-01-18 02:31:53 -0600

    completed navigation bar in the header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+//import { Navbar, NavbarBrand, NavItem, Nav } from 'reactstrap';
 
 const Header = () => (
-  <header>
-    <h1>Patrick site blah blah</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/gallery" activeClassName="is-active">Gallery</NavLink>
-    <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>    
-  </header>
+  <nav className="navbar navbar-light navbar-expand-sm bg-dark">
+    <NavLink className="navbar-brand nav-link text-light" to="/" activeClassName="active" exact={true}>Beard's Designs</NavLink>
+    <ul className="navbar-nav">
+      <li>
+        <NavLink className="nav-link text-light" to="/gallery" activeClassName="active">Gallery</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link text-light" to="/contact" activeClassName="active">Contact</NavLink>
+      </li>
+    </ul>
+  </nav>
 );
 
+//<NavLink to="/" activeClassName="is-active" exact={true}>Beard's Designs</NavLink>
+//<NavLink to="/gallery" activeClassName="is-active">Gallery</NavLink>
+//<NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
 export default Header;
