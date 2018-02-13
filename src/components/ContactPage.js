94279093afed8577865ebc18e87c942ce500f01e
hometownzero <bradycpeters@gmail.com>
2018-02-12 20:01:48 -0600

    trying out formik for the contact form, basic skeleton laid out need to add validation and start looking at mail api's

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,44 +1,54 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import { withFormik, Form, Field } from 'formik';
+import Yup from 'yup';
+import { render } from 'react-dom';
 
-const ContactPage = () => (
+const ContactPage = ({
+  values,
+}) => (
   <div>
-    <div className="contact-container">
-      <div className="contact-form">
-        <form>
-          <div className="input-half">
-            <input name="fromName" placeholder="Your Name" type="text"/>
-          </div>
-          <div className="input-half">
-            <input name="fromEmail" placeholder="Your Email" type="email" />
-          </div>
-          <div className="input-half">
-            <input name="fromPhone" placeholder="Your Phone Number" type="text"/>
-          </div>
-          <div className="input-half">
-            <input name="fromSubject" placeholder="Subject" type="text"/>
-          </div>
-          <div className="input-full">
-            <textarea name="messageBody" placeHolder="Your Message" />
-          </div>
-          <input type="submit" value="Send Message" />
-        </form>
-      </div>
-      <div className="contact-info">
-        <h1>Patrick Beard</h1>
-        <hr />
-        <div className="contact-info_text-block">
-          <h2> Email </h2>
-          <hr />
-          <p> blah@blah.blah </p>
-          <h2> Phone </h2>
-          <hr />
-          <p> 555-555-5555 </p>
-        </div>
-      </div>
-    </div>
-    <Footer />
+    <Form>
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="text" name="phone" placeholder="Phone Number" />
+      <Field type="text" name="name" placeholder="Name" />
+      <Field type="text" name="message" placeholder="Message" />
+      <button type="submit">Submit</button>
+    </Form>
   </div>
 );
 
-export default ContactPage;
+const ContactFormik = withFormik({
+  mapPropsToValues({ email, phone, name, message }) {
+    return {
+      email: email || '',
+      phone: phone || '',
+      name: name || '',
+      message: message || '',
+    }
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(ContactPage);
+
+export default ContactFormik;
+
+
+  // <div>
+  //   <div className="contact-container">
+  //     <div className="contact-info">
+  //       <h1>Patrick Beard</h1>
+  //       <hr />
+  //       <div className="contact-info_text-block">
+  //         <h2> Email </h2>
+  //         <hr />
+  //         <p> blah@blah.blah </p>
+  //         <h2> Phone </h2>
+  //         <hr />
+  //         <p> 555-555-5555 </p>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <Footer />
+  // </div>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import Header from './../components/Header';
 import HomePage from './../components/HomePage';
 import GalleryPage from './../components/GalleryPage';
-import ContactPage from './../components/ContactPage';
+import ContactFormik from './../components/ContactPage';
 import NotFoundPage from './../components/NotFound';
 
 const AppRouter = () => (
@@ -13,7 +13,7 @@ const AppRouter = () => (
     <Switch>
       <Route path="/" component={HomePage} exact={true} />
       <Route path="/gallery" component={GalleryPage} />
-      <Route path="/contact" component={ContactPage} />
+      <Route path="/contact" component={ContactFormik} />
       <Route path="" component={NotFoundPage} />
     </Switch>
   </div>
